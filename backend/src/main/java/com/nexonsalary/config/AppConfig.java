36478883f1713f7c3023510e71c984b640a4398e
amit michiner <amichiner@gmail.com>
2026-04-04 11:35:32 +0300

    feat: implement MonthlyBalanceService, update Hibernate configuration, and refactor DTOs while removing obsolete Jersey configuration and frontend components.

--- backend/src/main/java/com/nexonsalary/config/AppConfig.java
+++ backend/src/main/java/com/nexonsalary/config/AppConfig.java
@@ -1,12 +1,16 @@
 package com.nexonsalary.config;
 
+import com.nexonsalary.controller.BalanceImportController;
 import com.nexonsalary.controller.EmployeeController;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class AppConfig extends ResourceConfig {
 
     public AppConfig() {
         register(EmployeeController.class);
+        register(BalanceImportController.class);
+        register(MultiPartFeature.class);
         register(CorsFilter.class);
         packages("org.glassfish.jersey.jackson");
     }
